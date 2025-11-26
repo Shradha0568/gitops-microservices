6dffbb9bee2a9bcf1c6be40d1d19ad7cf968ccd7
shradha0568 <shradha.arsewar@neutrinotechlabs.com>
2025-11-26 13:02:52 +0000

    task completing

--- service-a/app.py
+++ service-a/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Service A Updated after chaging token"
+    return "Hello from Service A after task completion"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
