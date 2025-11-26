fdde6276c98aa5303e827c3e4db3753ef7ccadfb
shradha0568 <shradha.arsewar@neutrinotechlabs.com>
2025-11-26 12:30:42 +0000

    changed token

--- service-a/app.py
+++ service-a/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Service A Updated abc"
+    return "Hello from Service A Updated after chaging token"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
