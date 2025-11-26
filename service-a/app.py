c571bf0f9ce76bff3bbf6751cae6e447a4f50147
shradha0568 <shradha.arsewar@neutrinotechlabs.com>
2025-11-26 10:58:53 +0000

    fixed docker login issue

--- service-a/app.py
+++ service-a/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Service A Updated!!!"
+    return "Hello from Service A Updated abc"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
