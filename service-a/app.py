6ca4986ecc526281f4fa96897f832be056117a9f
shradha0568 <shradha.arsewar@neutrinotechlabs.com>
2025-11-27 07:46:06 +0000

    Checking

--- service-a/app.py
+++ service-a/app.py
@@ -6,5 +6,5 @@ def home():
     return "Hello from Service A after task completion finally all working hope so"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=80)
 
