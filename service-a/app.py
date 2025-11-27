30dc05e80982f40730508057443ebb2ad4926178
shradha0568 <shradha.arsewar@neutrinotechlabs.com>
2025-11-27 07:23:48 +0000

    Fix deployments and add dynamic tag update

--- service-a/app.py
+++ service-a/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Service A after task completion"
+    return "Hello from Service A after task completion finally all working hope so"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
