993530fd72d73f11cfb9c42985a4446bf6d39bc9
shradha0568 <shradha.arsewar@neutrinotechlabs.com>
2025-11-27 06:59:07 +0000

    Chnaged in service b for triggerring ci

--- service-b/app.py
+++ service-b/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Service B changed some thing"
+    return "Hello from Service B changed some thing for cheking"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
