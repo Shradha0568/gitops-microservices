15885c5421f43932b690909b1c620b652c5fa5ea
shradha0568 <shradha.arsewar@neutrinotechlabs.com>
2025-12-02 08:18:57 +0000

    checking image tag

--- service-a/app.py
+++ service-a/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Service A after task completion finally workkkkkkkkkkk"
+    return "Hello from Service A after task completion finally working version 38"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
