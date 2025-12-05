7efdd102ecc97bfe8373d0a620cddd31461a2d57
shradha0568 <shradha.arsewar@neutrinotechlabs.com>
2025-12-05 07:58:27 +0000

    checking for demo 13

--- service-a/app.py
+++ service-a/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Service A after task completion finally working version 40 please run"
+    return "Hello from Service A after task completion finally working version 40 please run please"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
